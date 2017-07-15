5a1583b9a4993474b6879457d97594963101d2d6
Ahmed Atef <Ahmed@Ahmeds-MacBook-Pro-9.local>
2017-07-15 02:24:30 +0200

    Modified the server.rb to have initialization to database [using in memory for testing] and added the route to getDep which is my function that should search for the dependencies

--- api/server.rb
+++ api/server.rb
@@ -3,12 +3,25 @@ require 'sinatra'
 require 'bundler'
 
 Bundler.require
+require 'rgem'
+require 'library'
 
-get '/' do
-    
+DataMapper.setup(:default, 'sqlite::memory:')
+DataMapper.finalize
+DataMapper.auto_migrate!
+
+get '/rgems' do
+
+     content_type :json
+     rgems = Rgem.all
+     rgems.to_json    
 end
 
 
-
+post '/getDep' do 
+	content_type :json
+	puts params[:gems]
+     
+end
 
 
